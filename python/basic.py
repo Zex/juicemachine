c55e55cda2373f113db5adb11e17272dc5687a70
zex <top_zlynch@yahoo.com>
2015-02-16 00:38:52 +0800

    add basic definition

---
+++ python/basic.py
@@ -0,0 +1,9 @@
+#!/utsr/bin/python
+## /setion basic.py
+# Common definition
+#
+# Author: Zex <top_zlynch@yahoo.com>
+#
+JM_SERVICE_NAME = "org.juicemachine"
+JM_1_IFACE  = "org.juicemachine.1.iface"
+JM_1_PATH  = "/org/juicemachine/1"
