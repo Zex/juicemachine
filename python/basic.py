dae44c4a8713bf1db6b0febc957ba8afb88060f4
zex <top_zlynch@yahoo.com>
2015-02-16 00:52:34 +0800

    rename with CONFIG

--- python/basic.py
+++ python/basic.py
@@ -5,5 +5,5 @@
 # Author: Zex <top_zlynch@yahoo.com>
 #
 JM_SERVICE_NAME = "org.juicemachine"
-JM_1_IFACE  = "org.juicemachine.config.iface"
-JM_1_PATH  = "/org/juicemachine/config"
+JM_CONFIG_IFACE  = "org.juicemachine.config.iface"
+JM_CONFIG_PATH  = "/org/juicemachine/config"
