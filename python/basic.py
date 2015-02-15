dd89323a52c20d74d4e0d35ebf448303eb2125ea
zex <top_zlynch@yahoo.com>
2015-02-16 00:42:01 +0800

    add config interface

--- python/basic.py
+++ python/basic.py
@@ -5,5 +5,5 @@
 # Author: Zex <top_zlynch@yahoo.com>
 #
 JM_SERVICE_NAME = "org.juicemachine"
-JM_1_IFACE  = "org.juicemachine.1.iface"
-JM_1_PATH  = "/org/juicemachine/1"
+JM_1_IFACE  = "org.juicemachine.config.iface"
+JM_1_PATH  = "/org/juicemachine/config"
