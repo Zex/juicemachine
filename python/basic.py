19b38404c07841fde4b6eca410f404e900030020
zex <top_zlynch@yahoo.com>
2015-02-20 08:59:33 +0800

    Change iface name for dev path accessing, add json path

--- python/basic.py
+++ python/basic.py
@@ -5,8 +5,10 @@
 # Author: Zex <top_zlynch@yahoo.com>
 #
 JM_SERVICE_NAME = "org.juicemachine"
-JM_CONFIG_IFACE  = "org.juicemachine.config.iface"
+JM_DEV_IFACE  = "org.juicemachine.dev.iface"
+JM_JSON_IFACE  = "org.juicemachine.json.iface"
 JM_CONFIG_PATH  = "/org/juicemachine/config"
 
-
 NVRAM_DEV = "/dev/nvram"
+JSON_PATH = "/tmp/juicemachine.json"
+
