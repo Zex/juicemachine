623e0109352fa5a0ec3edac1c4c80c5a74c646b7
zex <top_zlynch@yahoo.com>
2015-02-20 09:00:06 +0800

    add json access iface

--- python/jm_server.py
+++ python/jm_server.py
@@ -9,6 +9,8 @@ import dbus
 import dbus.service
 import dbus.mainloop.glib
 import gobject
+
+import json
 from basic import * 
 
 class JuiceMachine(dbus.service.FallbackObject):
@@ -24,7 +26,7 @@ class JuiceMachine(dbus.service.FallbackObject):
         dbus.service.Object.__init__(self, connection_name,
             JM_CONFIG_PATH)
 
-    @dbus.service.method(JM_CONFIG_IFACE,
+    @dbus.service.method(JM_DEV_IFACE,
             in_signature = '', out_signature = 's',
             path_keyword = 'path')
     def list(self, path = JM_CONFIG_PATH):
@@ -34,6 +36,22 @@ class JuiceMachine(dbus.service.FallbackObject):
         buf = ""
         try:
             with open(NVRAM_DEV, 'rw') as fd:
+                buf = fd.read()
+        except Exception as e:
+            buf = str(e)
+
+        return buf
+
+    @dbus.service.method(JM_JSON_IFACE,
+            in_signature = '', out_signature = 's',
+            path_keyword = 'path')
+    def list(self, path = JM_CONFIG_PATH):
+        """
+        TODO: Return goods list
+        """
+        buf = ""
+        try:
+            with open(JSON_PATH, 'rw') as fd:
                 buf = fd.read()
         except Exception as e:
             buf = str(e)
