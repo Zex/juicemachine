c53da27de7c8f7c7eba42125ef85ee19c2cac343
zex <top_zlynch@yahoo.com>
2015-02-21 18:55:25 +0800

    invoke get-name iface

--- python/jm_client.py
+++ python/jm_client.py
@@ -17,18 +17,25 @@ def start_request():
     Start sending requests to server
     """
     connection = dbus.SessionBus()
+
     obj = connection.get_object(
         JM_SERVICE_NAME,
         JM_CONFIG_PATH)
 
-    dev_iface = dbus.Interface(obj,
-        JM_DEV_IFACE)
+    print obj.Introspect()
+
+#    dev_iface = dbus.Interface(obj,
+#        JM_DEV_IFACE)
+#    print dev_iface.list()
 
     json_iface = dbus.Interface(obj,
         JM_JSON_IFACE)
 
-    print obj.Introspect()
-    print dev_iface.list()
     print json_iface.list()
+
+    get_name_iface = dbus.Interface(obj,
+        JM_GET_NAME_IFACE)
+
+    print get_name_iface.get_name()
 
 start_request()
