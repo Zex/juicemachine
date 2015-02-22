c532b5ae7de28cb8df4d9197c93cfc50afe5aa11
zex <top_zlynch@yahoo.com>
2015-02-22 23:05:53 +0800

    remove GET prefix from iface name, add id iface def

--- python/basic.py
+++ python/basic.py
@@ -4,11 +4,13 @@
 #
 # Author: Zex <top_zlynch@yahoo.com>
 #
-JM_SERVICE_NAME = "org.juicemachine"
-JM_DEV_IFACE  = "org.juicemachine.dev.iface"
-JM_JSON_IFACE  = "org.juicemachine.json.iface"
-JM_CONFIG_PATH  = "/org/juicemachine/config"
-JM_GET_NAME_IFACE  = "org.juicemachine.get.name.iface"
+JM_SERVICE_NAME     = "org.juicemachine"
+JM_DEV_IFACE        = "org.juicemachine.dev.iface"
+JM_JSON_IFACE       = "org.juicemachine.json.iface"
+JM_CONFIG_PATH      = "/org/juicemachine/config"
+
+JM_NAME_IFACE   = "org.juicemachine.name.iface"
+JM_ID_IFACE     = "org.juicemachine.id.iface"
 
 NVRAM_DEV = "/dev/nvram"
 JSON_PATH = "/tmp/juicemachine.json"
