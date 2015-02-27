740d314a14644f3958b32bd2be0a48f135b524d5
zex <top_zlynch@yahoo.com>
2015-02-27 23:05:22 +0800

    add settings manager

--- python/basic.py
+++ python/basic.py
@@ -1,5 +1,5 @@
-#!/utsr/bin/python
-## /setion basic.py
+#!/usr/bin/python
+# basic.py
 # Common definition
 #
 # Author: Zex <top_zlynch@yahoo.com>
@@ -9,9 +9,51 @@ JM_DEV_IFACE        = "org.juicemachine.dev.iface"
 JM_JSON_IFACE       = "org.juicemachine.json.iface"
 JM_CONFIG_PATH      = "/org/juicemachine/config"
 
-JM_NAME_IFACE   = "org.juicemachine.name.iface"
-JM_ID_IFACE     = "org.juicemachine.id.iface"
+JM_NAME_IFACE       = "org.juicemachine.name.iface"
+JM_ID_IFACE         = "org.juicemachine.id.iface"
 
-NVRAM_DEV = "/dev/nvram"
-JSON_PATH = "/tmp/juicemachine.json"
+NVRAM_DEV   = "/dev/nvram"
+JSON_PATH   = "/tmp/juicemachine.json"
+
+LOG_PREF    = "LOG>"
+
+def LOG(*msg):
+
+    print(LOG_PREF, *msg)
+
+
+class Settings:
+    """
+    Settings
+
+    'auto'      => boolean
+    'name'      => string
+    'url'       => string
+    'allows'    => list of string
+    """
+
+    __slots__ = [
+        'auto',
+        'name',
+        'url',
+        'allows'
+        ]
+
+    def __init__(self):
+
+        self.auto = True
+        self.name = ""
+        self.url = ""
+        self.allows = []
+
+    def __call__(self, *args):
+
+        try:
+            
+            [LOG(getattr(self, x)) for x in self.__all__]
+
+        except Exception as e:
+
+            LOG("ERR!", e)
+
 
