464faf3279eccce37f98819747605cc9078d699c
zex <top_zlynch@yahoo.com>
2015-03-02 00:25:38 +0800

    add ty restriction

--- python/common.py
+++ python/common.py
@@ -6,25 +6,52 @@
 # Author: Zex <top_zlynch@yahoo.com>
 #
 
+class string_ty(property):
+
+    __value = ''
+
+    def __set__(self, name, entry):
+
+        if not isinstance(entry, str):
+            raise TypeError('string value is required')
+
+        self.__value = entry
+
+    def __get__(self, *args):
+        return self.__value
+
+class integer_ty(property):
+
+    __value = int()
+
+    def __set__(self, name, entry):
+
+        if not isinstance(entry, int):
+            raise TypeError('integer value is required')
+
+        self.__value = entry
+
+    def __get__(self, *args):
+        return self.__value
+
 class CommonJM(object):
     """
     Common data structure definitions for juicemachine
     """
     __slots__ = [
-        '__name',
         ]
+        
     def __init__(self):
-        self.__name = ''
+        pass
 
-    @property
-    def name(self):
-        return self.__name
+    @string_ty
+    def name(self): pass
 
-    @name.setter
-    def name(self, entry):
-        if not isinstance(entry, str):
-            raise TypeError('string value is required')
-        self.__name = entry
+    @string_ty
+    def identity(self): pass
+    
+    @integer_ty
+    def count(self): pass
 
 def selftest():
 
@@ -38,9 +65,21 @@ def selftest():
     except Exception as e:
         print(e)
 
-    c.dummy = 568
+    try:
+        c.dummy = 568
+    except Exception as e:
+        print(e)
+    print('id:',c.identity)
+    c.identity = 'kjhgfd'
+    print('id:',c.identity)
+    print('count:',c.count)
+    print(type(c.count))
+    c.count = 1234567
+    print('count:',c.count)
+
 
 if __name__ == '__main__':
 
     selftest()
+
 
