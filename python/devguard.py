86609d7133a26c19713bdc786ba523f180eda64f
zex <top_zlynch@yahoo.com>
2015-03-11 23:17:27 +0800

    initial device guard

---
+++ python/devguard.py
@@ -0,0 +1,21 @@
+import pyudev
+from pyudev import MonitorObserver
+from gi.overrides import GObject as gobject
+
+def device_changed():
+
+    from os import mkdir
+
+    mkdir('/tmp/device-changed')
+
+context = pyudev.Context()
+monitor = pyudev.Monitor.from_netlink(context)
+monitor.filter_by('nvram')
+observer = MonitorObserver(monitor, device_changed)
+#observer.connect('device-changed', device_changed)
+#observer.start()
+
+#monitor.enable_receiving()
+#gobject.MainLoop().run()
+
+
