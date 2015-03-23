64f03c462934bbf92b99156aabaeea70bae8c1e9
zex <top_zlynch@yahoo.com>
2015-03-23 23:03:47 +0800

    list disk device

--- python/devguard.py
+++ python/devguard.py
@@ -19,13 +19,20 @@ def on_dev_changed(action, device):
         [fd.write(str(item)+'\n') for item in device.items()]
 
 context = pyudev.Context()
+
+for dev in context.list_devices(DEVTYPE='disk'):
+    for attr in ['device_node', 'device_number', 
+        'device_path', 'device_type', 'driver']:
+        print(attr, getattr(dev, attr))
+    print('device_links', [l for l in dev.device_links])
+    print('-------------------------------------')
+
 monitor = pyudev.Monitor.from_netlink(context)
 monitor.filter_by('block')
 
 observer = GUDevMonitorObserver(monitor)
 observer.connect('device-changed', on_dev_changed)
 monitor.start()
-
 gobject.MainLoop().run()
 
 
